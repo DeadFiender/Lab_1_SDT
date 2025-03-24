6671dbc6bf0707c7d903c800b2b4d8b3efd6065a
DeadFiender <artem.bibekin@gmail.com>
2025-03-24 12:49:40 +0700

    PathFile

--- main.cpp
+++ main.cpp
@@ -11,10 +11,14 @@ int main(int argc, char *argv[]){
     FileManager& manager = FileManager::instance();// Получаем экземпляр FileManager
     manager.setLogger(&logger);// Устанавливаем логгер для FileManager, чтобы он мог выводить сообщения
 
-    manager.addFile("C:/Users/bibek/Desktop/Test/a.txt");// Указываем путь к файлу
-    manager.addFile("C:/Users/bibek/Desktop/Test/b.txt");
-    manager.addFile("C:/Users/bibek/Desktop/Test/c.txt");
-    manager.addFile("C:/Users/bibek/Desktop/Test/c.txt");
+    //manager.addFile("C:/Users/bibek/Desktop/Test/a.txt");// Указываем путь к файлу
+    //manager.addFile("C:/Users/bibek/Desktop/Test/b.txt");
+    //manager.addFile("C:/Users/bibek/Desktop/Test/c.txt");
+    //manager.addFile("C:/Users/bibek/Desktop/Test/c.txt");
+    manager.addFile("H:/Desktop/Test/a.txt");
+    manager.addFile("H:/Desktop/Test/b.txt");
+    manager.addFile("H:/Desktop/Test/c.txt");
+    manager.addFile("H:/Desktop/Test/c.txt");
 
     manager.startMonitoring();// Запуск наблюдения за файлом
 
